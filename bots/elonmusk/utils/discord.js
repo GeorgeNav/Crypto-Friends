8fea51d701cce6c5be197af6eaa8be1afac159ee
GeorgeNav <george.g.navarro@gmail.com>
2021-05-21 22:12:13 -0500

    figuring out issue

--- bots/elonmusk/utils/discord.js
+++ bots/elonmusk/utils/discord.js
@@ -2,6 +2,7 @@ const { Client } = require('discord.js')
 const discord = new Client()
 const auth = require('../config/auth.json')
 
+console.log(auth.discord.ELON_TOKEN)
 discord.login(auth.discord.ELON_TOKEN)
 
 module.exports = discord
