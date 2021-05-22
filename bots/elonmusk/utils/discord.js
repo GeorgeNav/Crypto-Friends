dd95dc0e6f68d53a0b81dccb0ad3b868b48ed447
GeorgeNav <george.g.navarro@gmail.com>
2021-05-21 22:12:45 -0500

    remove useless log

--- bots/elonmusk/utils/discord.js
+++ bots/elonmusk/utils/discord.js
@@ -2,7 +2,6 @@ const { Client } = require('discord.js')
 const discord = new Client()
 const auth = require('../config/auth.json')
 
-console.log(auth.discord.ELON_TOKEN)
 discord.login(auth.discord.ELON_TOKEN)
 
 module.exports = discord
